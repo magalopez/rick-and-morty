2c3a5322d914cb15d991095e914874a446ae62de
Maria Gabriela Lopez <47748533+magalopez@users.noreply.github.com>
2021-09-20 23:05:53 -0500

    feat: new custom hook useCharacters

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useContext, useEffect, useReducer } from 'react'
-import Card from './Card'
+import React, { useState, useContext, useReducer, useMemo, useRef, useCallback } from 'react';
+import Card from './Card';
 import DarkModeContext from '../context/DarkModeContext';
+import Search from './Search';
+import useCharacters from '../hooks/useCharacters';
 
+const API = "https://rickandmortyapi.com/api/character/";
 const initialState = {
   favorites : []
 }
@@ -19,26 +22,29 @@ const favoriteReducer = (state, action) => {
 }
 
 const Characters = () => {
-  const [ characters, setCharacters ] = useState([])
-  const [ state, dispatch ] = useReducer(favoriteReducer, initialState)
+  const characters = useCharacters(API)
+  const [ search , setSearch ] = useState("")
   const { darkMode } = useContext(DarkModeContext)
+  const [ state, dispatch ] = useReducer(favoriteReducer, initialState)
   const { favorites } = state
-
-  useEffect(() =>{
-    fetch("https://rickandmortyapi.com/api/character/")
-    .then(response => response.json())
-    .then(data => {
-      console.log(`data`, data)
-      setCharacters(data.results)
-    })
-  },[])
+  const searchInput = useRef(null)
 
   const handleAddFavorite = favorite => {
     dispatch({ type: 'ADD_TO_FAVE', payload : favorite})
   }
 
+  const handleSearch = useCallback(() => {
+    setSearch(searchInput.current.value)
+  }, [])
+
+  const filteredCharacters = useMemo(() => characters.filter(character => character.name.toLowerCase().includes(search.toLowerCase())), [characters, search]) 
+
   return (
     <div className="container">
+      <Search
+        search={search}
+        searchInput={searchInput}
+        handleSearch={handleSearch} />
       {favorites.length > 0 && (
         <div>
           <h2>Favorites Characters</h2>
@@ -51,7 +57,7 @@ const Characters = () => {
       <div>
         <h2>Characters</h2>
         <div className={`characters characters--${darkMode ? "dark" : "light"}`}>
-          {characters.map((character, index) => (
+          {filteredCharacters.map((character, index) => (
             <Card 
             customClass={darkMode ? "dark" : "light"}
             key={index}
